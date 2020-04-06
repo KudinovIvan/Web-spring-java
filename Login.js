cbc864809ebfc909ed96f0341805521ac6056f45
KudinovIvan <marvel.2012@mail.ru>
2020-04-06 17:44:45 +0300

    Update

--- Login.js
+++ Login.js
@@ -48,24 +48,24 @@ Ext.application({
                 click: function(){
 
                     if ((Ext.getCmp("login").getValue()=="Admin")&&(Ext.getCmp("password").getValue()=="123456"))
-                  /* Ext.Ajax.request({
+                   Ext.Ajax.request({
                        url: 'JDBCPostgreSQLExample.java',
-                    //   method: 'POST',
+                       method: 'POST',
                        params:{
                               username: Ext.getCmp("login").getValue(),
                               password: Ext.getCmp("password").getValue()
                        },
 
                          success: function(response, opts) {
-                               var obj = Ext.decode(response.responseText);
-                               console.dir(obj);
+                              // var obj = Ext.decode(response.responseText);
+                               console.dir(response);
                            },
                        failure: function(response, options){
                                alert("Ошибка: " + response.statusText);
                        }
                    })
-                  */
-                 window.location.assign('Window.html');
+
+                 //window.location.assign('Window.html');
                     else
                     alert("Данного пользователя нет в системе");
                 }
